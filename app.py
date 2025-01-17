bce16adc6b634cbc686cee2f0ffa695e92f909a0
philbiju <117933174+philbiju@users.noreply.github.com>
2025-01-17 17:35:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/solutions")
+def solutions():
+    return render_template("solutions.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
